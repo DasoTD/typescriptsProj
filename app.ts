6625074c37cef44bcf1605a7b679cbc9503c1276
Daso <daso.timileyin@gmail.com>
2022-01-04 10:58:46 +0100

    nodemon

--- app.ts
+++ app.ts
@@ -1,6 +1,8 @@
 import express, {Request, Response} from 'express'
 
 const App = express()
+App.use(express.json())
+App.use(express.urlencoded({extended:true}))
 const PORT = process.env.PORT || 3000
 
 App.listen(PORT, () => {
@@ -10,3 +12,5 @@ App.listen(PORT, () => {
 App.get('/', (req: Request, res:Response) => {
     res.send("Na typescript sure pass maphor")
 })
+
+console.log("hi Timmy....");
